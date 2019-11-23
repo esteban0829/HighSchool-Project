ca59eb5a39631c06a8f4cc764c9b4fabaf8f6dd6
esteban0829 <46747617+esteban0829@users.noreply.github.com>
2019-11-24 00:53:14 +0900

    Update arduino_Uno_serial_transmit.ino

--- arduino_Uno_mega_serial_transmit/arduino_Uno_serial_transmit/arduino_Uno_serial_transmit.ino
+++ arduino_Uno_mega_serial_transmit/arduino_Uno_serial_transmit/arduino_Uno_serial_transmit.ino
@@ -5,10 +5,19 @@ void setup() {
   pinMode(13,OUTPUT);
 }
 
-int input;
-void loop() {
-  Serial.write(1);
-  blinkLed();
+char input;
+void loop(){
+  if(Serial.available()){
+    blinkLed();
+    input=Serial.read();
+    if(input=='O'){
+      Serial.println("Door is Opened");
+    }else if(input=='M'){
+      Serial.println("Manager Opened Door");
+    }else if(input=='X'){
+      Serial.println("Wrong number Detected 3 Times");
+    }
+  }
 }
 
 void blinkLed(){
